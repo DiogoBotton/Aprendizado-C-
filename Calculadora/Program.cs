aece8a6e8bfa95f4104384d63efd35a19286ece6
DiogoBotton <diogo.luo@gmail.com>
2019-09-18 17:34:19 -0300

    First commit

---
+++ Calculadora/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 0;
+            float num2 = 0;
+
+            string operador;
+
+            Console.WriteLine("Digite um número.");
+            num1 = Convert.ToInt32 (Console.ReadLine()); //int.Parse (Converte automáticamente)
+
+            Console.WriteLine("Digite outro número.");
+            num2 = Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine("Digite o operador aritmético.");
+            Console.WriteLine("Escreva exatamente desta forma: divisao, multiplicacao, soma, subtracao");
+            operador = Console.ReadLine();
+
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2} ");
+            Console.WriteLine($"{num1} + {num2} = {num1 - num2} ");
+            Console.WriteLine($"{num1} + {num2} = {num1 / num2} ");
+            Console.WriteLine($"{num1} + {num2} = {num1 * num2} ");
+            Console.WriteLine($"{num1} + {num2} = {num1 % num2} ");
+            
+            /*string divisao;
+            string multiplicacao;
+            string soma;
+            string subtracao;
+
+            if (operador == soma)
+            {
+                Console.WriteLine("O Resultado é: " + num1 + num2)
+            }*/
+
+            
+
+
+        }
+    }
+}
---
+++ Media/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 0;
+            float num2 = 0;
+            float num3 = 0;
+            float num4 = 0;
+
+            Console.WriteLine("Digite o 1° número");
+            num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o 2° número");
+            num2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o 3° número");
+            num3 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o 4° número");
+            num4 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{num1} + {num2} + {num3} + {num4} / 4 = {num1 + num2 + num3 + num4 / 4}");
+
+        }
+    }
+}
