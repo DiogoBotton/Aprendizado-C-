3cd0547c655983c088c51385d3f8820c91d01de9
DiogoBotton <diogo.luo@gmail.com>
2019-09-20 17:54:53 -0300

    Tabuada 5

--- Tabuada/Program.cs
+++ Tabuada/Program.cs
@@ -7,12 +7,19 @@ namespace Tabuada
         static void Main(string[] args)
         {
             //Criar aplicação que imprima impares inteiros de 0 a 100
-            for (int num = 1; num < 100; num += 2)
+
+            /*for (int num = 1; num < 100; num += 2)
             {
                 Console.WriteLine(num);
-            }
+            }*/
+
             // À fazer:
             //Tabuada do 5 usando FOR
+
+            for (int num = 1; num <= 10; num++)
+            {
+                Console.WriteLine(num * 5);
+            }
 
             //Tabuada do 1 ao 10 usando FOR
         }
