beead01045dea3b76ebc1ace8545cd4ae7b6d156
DiogoBotton <diogo.luo@gmail.com>
2019-09-23 23:35:49 -0300

    registro e login

--- loginEsenha/Program.cs
+++ loginEsenha/Program.cs
@@ -1,34 +1,64 @@
 ﻿using System;
 
-namespace loginEsenha
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-           
+namespace loginEsenha {
+    class Program {
+        static void Main (string[] args) {
+
             string login_adm = "admin";
             string senha_adm = "admin";
 
             string login = "";
             string senha = "";
 
-            Console.Write("Login:");
-            login = Console.ReadLine();
+            string registro_login = "";
+            string registro_senha = "";
 
-            Console.Write("Senha:");
-            senha = Console.ReadLine();
+            string resp;
 
-            if ((login == login_adm) && (senha == senha_adm))
-            {
-                Console.WriteLine("Você logou como ADMIN.");
+            Console.Write ("Digite seu Login:");
+            login = Console.ReadLine ();
+
+            Console.Write ("Digite sua Senha:");
+            senha = Console.ReadLine ();
+
+            if ((login == login_adm) && (senha == senha_adm)) {
+                Console.WriteLine ("Você logou como ADMIN.");
+            } else if ((login != registro_login) && (senha != registro_senha)) {
+                Console.WriteLine ("Login e senha inválidos ou não encontrado nas credênciais do programa.");
+                Console.WriteLine ("Deseja fazer um cadastro? Responda s / n (fechar programa).");
+                resp = Console.ReadLine ();
+
+                switch (resp) {
+                    case "s":
+                        Console.Write ("Crie um login:");
+                        registro_login = Console.ReadLine ();
+                        Console.Write ("Crie uma senha:");
+                        registro_senha = Console.ReadLine ();
+
+                        Console.WriteLine ("Você foi registrado com sucesso! Agora faça o seu login.");
+
+                        while ((login != registro_login) || (senha != registro_senha)) {
+
+                            Console.Write ("Digite seu Login:");
+                            login = Console.ReadLine ();
+
+                            Console.Write ("Digite sua Senha:");
+                            senha = Console.ReadLine ();
+
+                            if ((login != registro_login) || (senha != registro_senha)){
+                                Console.WriteLine("Login ou senha invalidos, tente novamente. ");
+                            }
+                        }
+                        if ((login == registro_login) && (senha == registro_senha)) {
+                            Console.WriteLine ("Seu login foi efetuado com sucesso.");
+                        }
+                        break;
+                    case "n":
+                        Console.Write ("O programa será finalizado.");
+                        break;
+                }
+
             }
-            else {
-                Console.WriteLine("Você logou como usuário.");
-            }
-        
-    
-                     
 
         }
     }
