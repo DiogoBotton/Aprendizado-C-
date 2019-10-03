53b77204166c4e0f3a23a3fed7dc17d603166134
DiogoBotton <diogo.luo@gmail.com>
2019-10-03 17:17:12 -0300

    exercicios revisao e bytebank

---
+++ ByteBank/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            //Cliente cliente = CriarCliente();
+            
+            ContaCorrente conta = CriarConta();
+            
+
+        }
+        public static Cliente CriarCliente(){
+            Console.WriteLine("Digite seu nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite seu CPF");
+            string cpf = Console.ReadLine();
+            Console.WriteLine("Digite seu Email");
+            string email = Console.ReadLine();
+
+            Cliente cliente = new Cliente(nome, cpf, email);
+            
+            string senha = "";
+            string validacaoSenha = "";
+
+            bool trocouSenha = false;
+
+            do{
+            Console.WriteLine("Digite uma senha");
+            senha = Console.ReadLine();
+
+            trocouSenha = cliente.TrocaSenha(senha); //Recebe o valor booleano da classe.
+            
+                if (trocouSenha == false){
+                    System.Console.WriteLine("Digite uma senha com mais de 6 caracteres e menor que 16.");
+                }
+                else {
+                    do{
+
+                    System.Console.WriteLine("Confirme sua senha.");
+                    validacaoSenha = Console.ReadLine();
+
+                    if (validacaoSenha == senha){
+                        System.Console.WriteLine("Sua conta foi cadastrada com sucesso!");
+                    }
+                    else{
+                        System.Console.WriteLine("Senhas não correspondem, digite novamente.");
+                    }
+                    }while (validacaoSenha != senha);
+                }
+            }while (trocouSenha != true);//ou (!trocouSenha)
+
+            return cliente;
+        }
+        public static ContaCorrente CriarConta(){
+            Console.WriteLine("Entre com o Titular:");
+            string titular = Console.ReadLine();
+            Console.WriteLine("Entre com o número da Agência:");
+            int agencia = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Entre com o número:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            ContaCorrente conta = new ContaCorrente(agencia, numero, titular);
+            
+            do{
+            Console.WriteLine("Entre com o Saldo da conta:");
+            conta.Saldo = Convert.ToDouble(Console.ReadLine());
+                if (conta.Saldo < 0){
+                    System.Console.WriteLine("Você não pode iniciar a conta com saldo negativo.");
+                }
+            }while (conta.Saldo < 0);
+
+            System.Console.WriteLine($"O saldo atual da conta é: R$ {conta.Saldo}. ");
+
+            return conta;
+        }
+    }
+}
---
+++ exerciciosMatriz/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace exerciciosMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1°
+            /*Console.WriteLine("Digite o tamanho do vetor.");
+            int tamanho = Convert.ToInt32(Console.ReadLine());
+
+            int[] numeros = new int[tamanho];
+
+            for(int i = 0; i < tamanho; i++){
+
+            System.Console.WriteLine($"Digite o {i + 1}° número.");
+            numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            System.Console.WriteLine("Os números são: ");
+            
+            int cont = 1;
+            foreach (int elemento in numeros){
+                System.Console.WriteLine($"{cont}° número: " + elemento);
+                cont++;
+            }*/
+
+            // 2°
+
+            int[] sorteio = new int[10];
+            int cont = 0;
+            
+
+            Random aleatorio = new Random();
+
+            foreach(int elemento in sorteio){   
+            sorteio[cont] = aleatorio.Next(0,50);
+            cont++;
+            }
+
+            System.Console.Write("Números pares: ");
+            for(int i = 0; i < sorteio.Length; i++){
+                if ((sorteio[i] % 2) == 0){
+                System.Console.Write(sorteio[i] + " ");
+                }
+            }
+            System.Console.WriteLine();
+
+            System.Console.Write("Números ímpares: ");
+            for(int i = 0; i < sorteio.Length; i++){
+                if ((sorteio[i] % 2) == 1){
+                System.Console.Write(sorteio[i] + " ");
+                }
+            }
+
+
+
+
+            
+
+        }
+    }
+}
